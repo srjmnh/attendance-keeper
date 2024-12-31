e7039c7140d91f34c56a301a9c621b5e39a81e7e
srjmnh <30594403+srjmnh@users.noreply.github.com>
2024-12-31 19:17:12 +0300

    get error

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const { registerStudent } = require('./students');
-const { saveAttendance } = require('./attendance');
+const { registerStudent, recognizeFace } = require('./students');
 
 const app = express();
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(express.static(path.join(__dirname, '../frontend')));
 
-// Routes
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../frontend/index.html')));
+// Serve the register.html file on /register route
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, '../frontend/register.html')));
+
+// Serve the recognize.html file on /recognize route
+app.get('/recognize', (req, res) => res.sendFile(path.join(__dirname, '../frontend/recognize.html')));
+
+// API routes
 app.post('/register-student', registerStudent);
-app.post('/record-attendance', saveAttendance);
+app.post('/recognize-face', recognizeFace);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
