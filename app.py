e02074405ac77e6ab6f016708773c416494b33ce
srjmnh <30594403+srjmnh@users.noreply.github.com>
2025-01-02 03:33:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -258,3 +258,4 @@ if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))  # Default port for Flask
     port = int(os.getenv('PORT', 5000))  # Use PORT from environment or default to 5000
     app.run(host='0.0.0.0', port=port)
+    
