c0140383dd938ede405f6eae5d8abb4a382a12ad
srjmnh <30594403+srjmnh@users.noreply.github.com>
2025-01-04 13:24:46 +0300

    test

--- static/script.js
+++ static/script.js
@@ -272,6 +272,12 @@ $(document).ready(function() {
     });
 
     loadSubjects();
+
+    // Initialize Bootstrap tooltips
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(function(tooltipTriggerEl) {
+        new bootstrap.Tooltip(tooltipTriggerEl)
+    });
 });
 
 function loadAttendance() {
